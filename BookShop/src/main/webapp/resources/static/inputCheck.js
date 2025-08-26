fabf817672680679eaef5d78157e4f2c38447e78
wlwl45wl <wlwl45wl@gmail.com>
2025-08-26 17:52:12 +0900

    2025.08.26

---
+++ BookShop/src/main/webapp/resources/static/inputCheck.js
@@ -0,0 +1,23 @@
+/** js  ->  jQuery   ->     React, Vue, Agular      ->      바닐라 JS
+ * 
+ * jQuery는 코드길이가 짧아짐
+ *      DOM에서 돌아감.
+ * 
+ * R.V.A
+ *      SPA?
+ *      웹 앱에 많이 사용됨.
+ *          앱 생태계는 돈이 안됨.
+ * 
+ * 바닐라JS
+ *      코드 길이가 길지만 툴이 많이 늘어남.
+ */
+
+//입력 확인
+
+window.addEventListener("load", () => {
+    //Id로 요소 선택
+    //해당 요소가 클릭됬을 때 이벤트 처리
+    document.getElementById("submit").addEventListener("click", e => {
+
+    });
+});
